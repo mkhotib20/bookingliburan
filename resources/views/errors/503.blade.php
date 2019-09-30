fb48876a1ee18278789aac27b3c15533e2f81e18
Muhammad khotib <khotib@docotel.com>
2019-09-30 13:09:00 +0700

    mintenaance mode

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{asset('public/front/logo-transparan.png')}}" type="image/png" />
+    <meta http-equiv="description" content="Our services are travvel planner, private trip. Just take care of your leave, we take care of the trip.">
+    <title>Minatenance</title>
+    <style>
+    *{
+        text-align: center;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    body{
+        margin-top: 100px;
+        padding: 20px;
+    }
+    </style>
+</head>
+<body>
+    <img src="{{asset('public/front/logo-transparan.png')}}" style="width: 100px" alt="booking_liburan_icon">
+    <p>Hello, we're bookingliburan</p>
+    <p>Our site is currently under construction</p>
+</body>
+</html>
